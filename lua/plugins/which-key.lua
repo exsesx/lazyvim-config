990761fdbe912fbf461ff974f523fcd0dcf9875b
Oleh Vanin <exsesx@gmail.com>
2024-05-17 12:39:33 +0200

    which-key fix group names (revert to 1.4); add gitsigns lua function call instead of vim commands; add treesitter textobjects; general clean up

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,11 +1,18 @@
 return {
   {
     "folke/which-key.nvim",
-    event = "VeryLazy",
+    -- FIXME: https://github.com/folke/which-key.nvim/issues/482
+    -- FIXME: https://github.com/LazyVim/LazyVim/issues/1955
+    -- FIXME: https://github.com/folke/which-key.nvim/issues/514
+    -- tl;dr versions >1.4 forbid to setup empty groups (like the ones below)
+    version = "1.4",
     opts = {
-      plugins = { spelling = true },
       defaults = {
         ["<leader>l"] = { name = "+lsp/lazy" },
+        ["<leader>gh"] = { name = "+hunks" },
+        ["<leader>gt"] = { name = "+toggle" },
+        ["<leader>n"] = { name = "+next" },
+        ["<leader>p"] = { name = "+prev" },
       },
     },
   },
