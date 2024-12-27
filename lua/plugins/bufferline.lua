1910a9c0a35f43e1d14922ee8b1c54492abc3444
Oleh Vanin <exsesx@gmail.com>
2024-12-27 12:36:49 +0100

    bufferline pin icon improvements

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,6 +10,11 @@ return {
         show_buffer_close_icons = false,
         show_close_icon = false,
         separator_style = { "|", "|" },
+        groups = {
+          items = {
+            require("bufferline.groups").builtin.pinned:with({ icon = "" }),
+          },
+        },
       },
     },
   },
